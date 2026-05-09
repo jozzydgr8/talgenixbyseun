c482f1b9277b2b1e83dfac484ad7c0544bb512ba
jozzydgr8 <jozzydgreat1@gmail.com>
2026-05-09 15:31:48 -0700

    adding social icons

--- src/pages/homepage/homeComponent/Hero.tsx
+++ src/pages/homepage/homeComponent/Hero.tsx
@@ -1,36 +1,51 @@
 import { useEffect } from "react";
 import { FlatButton } from "../../../shared/FlatButton";
-import {FacebookFilled} from '@ant-design/icons';
+import {FacebookFilled, LinkedinFilled, InstagramFilled} from '@ant-design/icons';
 export const Hero = ()=>{
     useEffect(() => {
-    const headerText = document.querySelector('.headerWrite');
+    const headerText = document.querySelector('.heroWrite');
     headerText?.classList.add('sectionAnimationLeft');
 
-    const headerButton = document.querySelector('.headerButton');
+    const headerButton = document.querySelector('.heroBottom');
     headerButton?.classList.add('sectionAnimationUp');
 
-    const headerBadge = document.querySelector('.header-badge');
-    headerBadge?.classList.add('sectionAnimationDown');
+    const heroBadge = document.querySelector('.heroBadge');
+    heroBadge?.classList.add('sectionAnimationDown');
     }, []);
     return(
         <section>
             <div className="container-fluid">
                 <div className="row">
                     <div className='col-md-6'>
-                        <h1 className="headerBadge">HAY! I'M SEUN OGUNSANYA</h1>
-                        <h4 className="headerWrite">
-                            DATA ANALYST CONSULTANT
-                        </h4>
+                        <h1 className="heroBadge">HEY! I'M SEUN OGUNSANYA</h1>
+                        <h1 className="heroWrite">
+                            DATA ANALYST CONSULTANT |
+                        </h1>
+                        <br/>
+                        <div className="heroBottom">
                         <p>
                             Transforming data into actionable insights for informed decision-making. With expertise in data analysis, visualization, and strategic consulting, I help businesses unlock the full potential of their data to drive growth and innovation.
                         </p>
 
-                        <div>
-                        <FlatButton title="Get In Touch" className=" btn btnPrimary btn-lg"/>
-                        <div>
-                            <a href="https://www.facebook.com/seun.ogunsanya.5" target="_blank" rel="noopener noreferrer">
-                                <FacebookFilled style={{ fontSize: '24px', color: '#3b5998' }} />
-                            </a>
+                        <div className="row align-items-start">
+                        <div className="col-md-3">
+                            <FlatButton title="Get In Touch" className=" btn btnPrimary btn-lg"/>
+                        </div>
+                        <div className="col-md-6">
+                            <div  style={{display:'flex', flexDirection:"row", gap:"10px"}}>
+                                <a href="/" target="_blank" rel="noopener noreferrer">
+                                    <FacebookFilled style={{ fontSize: '40px', color: '#3b5998' }} />
+                                </a>
+                                <a href="/" target="_blank" rel="noopener noreferrer">
+                                    <LinkedinFilled style={{ fontSize: '40px', color: '#0077B5' }} />
+                                </a>
+                                <a href="/" target="_blank" rel="noopener noreferrer">
+                                    <InstagramFilled style={{ fontSize: '40px', color: '#dc3545' }} />
+                                </a>
+                            </div>
+
+                        </div>
+                        
                         </div>
                         </div>
                     </div>
