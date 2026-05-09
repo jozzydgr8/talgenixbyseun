d504acbc4e2e051fe21b9d2d17558b7c2a9d89d0
jozzydgr8 <jozzydgreat1@gmail.com>
2026-05-09 14:48:16 -0700

    added hero section

--- src/pages/homepage/homeComponent/Hero.tsx
+++ src/pages/homepage/homeComponent/Hero.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from "react";
+import { FlatButton } from "../../../shared/FlatButton";
+import {FacebookFilled} from '@ant-design/icons';
 export const Hero = ()=>{
     useEffect(() => {
     const headerText = document.querySelector('.headerWrite');
@@ -13,7 +15,30 @@ export const Hero = ()=>{
     return(
         <section>
             <div className="container-fluid">
-                <h1 className="headerWrite">Hero</h1>
+                <div className="row">
+                    <div className='col-md-6'>
+                        <h1 className="headerBadge">HAY! I'M SEUN OGUNSANYA</h1>
+                        <h4 className="headerWrite">
+                            DATA ANALYST CONSULTANT
+                        </h4>
+                        <p>
+                            Transforming data into actionable insights for informed decision-making. With expertise in data analysis, visualization, and strategic consulting, I help businesses unlock the full potential of their data to drive growth and innovation.
+                        </p>
+
+                        <div>
+                        <FlatButton title="Get In Touch" className=" btn btnPrimary btn-lg"/>
+                        <div>
+                            <a href="https://www.facebook.com/seun.ogunsanya.5" target="_blank" rel="noopener noreferrer">
+                                <FacebookFilled style={{ fontSize: '24px', color: '#3b5998' }} />
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+
+                    </div>
+                </div>
+                
             </div>
         </section>
     )
