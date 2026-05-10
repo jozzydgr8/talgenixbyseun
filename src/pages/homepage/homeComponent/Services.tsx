6a79a2a83e3d7035853a42ce4969205fcb32f36d
jozzydgr8 <jozzydgreat1@gmail.com>
2026-05-10 01:36:06 -0700

    styled service section

--- src/pages/homepage/homeComponent/Services.tsx
+++ src/pages/homepage/homeComponent/Services.tsx
@@ -1,18 +1,21 @@
 import {servicesData} from '../../../shared/Data'
+import { FlatButton } from '../../../shared/FlatButton'
+import {SendOutlined} from '@ant-design/icons'
 export const Services = ()=>{
     return(
         <section>
             <div className="container-fluid">
                 <h2 className='homeHeader'>Explore Our Services</h2>
-                <br/><br/>
+                <br/>
                 <div className="row">
                     {servicesData.map((service, index)=>(
-                        <div className="col-md-3" key={index}>
-                            <div className='serviceCard'>
-                                <h3>{service.title}</h3>
+                        <div className="col-md-3" key={index} style={{marginBottom:'20px'}}>
+                            <div className='serviceCard d-flex flex-column h-100 animate-up'>
+                                <h3 className='home-subheader'>{service.title}</h3>
                                 <br/>
                                 <p>{service.description}</p>
                                 <br/>
+                                <FlatButton title={service.cta} className='borderlessbtn mt-auto text-start' icon={<SendOutlined />}/>
                                 
                             </div>
                         </div>
