c773028e4b5e511b0b7b2b536ba851405bbe7845
jozzydgr8 <jozzydgreat1@gmail.com>
2026-05-11 20:01:35 -0700

    editing hero section

--- src/pages/homepage/homeComponent/Hero.tsx
+++ src/pages/homepage/homeComponent/Hero.tsx
@@ -17,23 +17,24 @@ export const Hero = ()=>{
     heroBadge?.classList.add('sectionAnimationDown');
     }, []);
     return(
-        <section>
+        <section id="hero">
             <div className="container-fluid">
                 <div className="row align-items-center justify-content-center">
                     <div className='col-md-6'>
-                        <h1 className="heroBadge">HEY! I'M SEUN OGUNSANYA</h1>
+                        <h1 className="heroBadge">Transforming Data into </h1>
                         <h3 className="heroWrite">
-                            DATA ANALYST CONSULTANT 
+                            Actionable Insights
                         </h3>
                         <br/>
                         <div className="heroBottom">
                         <p>
-                            Transforming data into actionable insights for informed decision-making. With expertise in data analysis, visualization, and strategic consulting, I help businesses unlock the full potential of their data to drive growth and innovation.
+                            Help businesses harness the power of data anlytics, business intelligence, data visualization, and AI to make informed decisions, automate processes
+                            , and accelerate digital transformation..  
                         </p>
 
-                        <div className="row align-items-start">
-                        <div className="col-md-3">
-                            <FlatButton title="Get In Touch" className=" btn btnPrimary btn-lg"/>
+                        <div className="row align-items-center">
+                        <div className="col-md-4">
+                            <FlatButton title="Get In Touch" className=" btn btnPrimary btn-xl"/>
                         </div>
                         <div className="col-md-6">
                             <div  style={{display:'flex', flexDirection:"row", gap:"10px"}}>
@@ -47,8 +48,28 @@ export const Hero = ()=>{
                                     <InstagramFilled style={{ fontSize: '40px', color: '#dc3545' }} />
                                 </a>
                             </div>
+                            
 
                         </div>
+                        <hr/>
+                            <div style={{display:'flex', gap:"20px"}}>
+                                <div>
+                                    <h3>50+</h3>
+                                    <small>Projects Delivered</small>
+                                </div>
+                                <div>
+                                    <h3>30+</h3>
+                                    <small>Happy Clients</small>
+                                </div>
+                                <div>
+                                    <h3>10+</h3>
+                                    <small>Awards Won</small>
+                                </div>
+                                <div>
+                                    <h3>100%</h3>
+                                    <small>Client Satisfaction</small>
+                                </div>
+                            </div>
                         
                         </div>
                         </div>
