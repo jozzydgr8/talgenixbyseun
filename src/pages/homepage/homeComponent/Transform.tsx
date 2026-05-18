faa5b60fd163d417dd6befd180614ea777932268
jozzydgr8 <jozzydgreat1@gmail.com>
2026-05-18 00:18:58 -0700

    added transform section

--- src/pages/homepage/homeComponent/Transform.tsx
+++ src/pages/homepage/homeComponent/Transform.tsx
@@ -1,5 +1,31 @@
 import { businessDetails } from "../../../shared/Data"
+import {PhoneOutlined,LinkedinOutlined,TwitterOutlined,GithubOutlined,
+        MailOutlined,
+        ClockCircleOutlined} from '@ant-design/icons';
 
+const Styles = {
+    detailsContainer:{
+        backgroundColor:"white",
+        color:'black',
+        padding:'20px',
+        borderRadius:'12px',
+        margin:"10px 0",
+         display:'flex',
+         alignItems:'center',
+         gap:'12px'
+    },
+    iconContainer:{
+        minWidth: "52px",
+        height: "52px",
+        borderRadius: "12px",
+        color: "#005bb5",
+        background: "rgba(152, 222, 245, 0.8)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "18px"
+    }
+}
 export const Transform = ()=>{
     return(
         <section>
@@ -25,12 +51,59 @@ export const Transform = ()=>{
                                 <div>
                                     {businessDetails.map((detail, index) => (
                                         <div key={index}>
-                                            <h4>Email: </h4> <p>{detail.email}</p>
-                                            <h4>Phone: </h4> <p>{detail.phone}</p>
-                                            <h4>Business Hours: </h4> <p>{detail.business_hours.days} {detail.business_hours.hours}</p>
+
+                                           <div style={Styles.detailsContainer}>
+                                            <div style={Styles.iconContainer}>
+                                             <MailOutlined /> 
+                                             </div>
+                                             <div>
+                                                <h4>Email: </h4> <p>{detail.email}</p>
+                                            </div>
+                                            </div>
+
+                                            <div style={Styles.detailsContainer}>
+                                                <div
+                                                    style={Styles.iconContainer}
+                                                >
+                                                    <PhoneOutlined /> 
+                                                </div> 
+                                                
+                                                <div>
+                                                    <h4>Phone: </h4> <p>{detail.phone}</p>
+                                                </div>
+                                            </div>
+                
+                                            
+                                            <div style={Styles.detailsContainer}>
+                                                <div style={Styles.iconContainer}>
+                                                    <ClockCircleOutlined />
+                                                </div>
+                                                
+                                                 <div>
+                                                    <h4>Business Hours: </h4> <p>{detail.business_hours.days} {detail.business_hours.hours}</p>
+                                                 </div>
+                                            </div>
                                         </div>
                                     ))}
                                 </div>
+
+
+                                <div style={{backgroundColor:"#005bb5", padding:"30px", borderRadius:"12px"}}>
+                                    <h4>Connect with me</h4>
+                                    <p>Follow my work and stay updated on the latest in data analytics and AI.</p>
+                                    <div  style={{display:'flex', flexDirection:"row", gap:"10px"}}>
+                                        <a href="/" target="_blank" rel="noopener noreferrer">
+                                            <LinkedinOutlined style={{ fontSize: '20px', background: '#3b5998', color:"white" }} />
+                                        </a>
+                                        <a href="/" target="_blank" rel="noopener noreferrer">
+                                            <TwitterOutlined style={{ fontSize: '20px', background: '#0077B5', color:"white" }} />
+                                        </a>
+                                        <a href="/" target="_blank" rel="noopener noreferrer">
+                                            <GithubOutlined style={{ fontSize: '20px', color: '#dc3545' }} />
+                                        </a>
+                                </div>
+                                </div>            
+
                             </div>
                     </div>
                 </div>
